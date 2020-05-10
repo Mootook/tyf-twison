d893241cdbae7a0d6c185a6a076f58562237a647
Alex Mutuc <alexanderkmutuc@gmail.com>
2020-05-10 13:28:04 -0700

    Update npm dependencies, move shell cmd to successful on build from watch

--- build.js
+++ build.js
@@ -1,13 +1,15 @@
-var fs = require('fs')
-var Uglify = require('uglify-js')
+const fs = require('fs')
+const Uglify = require('uglify-js')
+const childProcess = require('child_process')
+const exec = childProcess.exec
 
-var package = JSON.parse(fs.readFileSync("package.json", "utf-8"))
-var html = fs.readFileSync("src/storyFormat.html", "utf-8")
-var js = Uglify.minify("src/twison.js")
+const package = JSON.parse(fs.readFileSync("package.json", "utf-8"))
+const html = fs.readFileSync("src/storyFormat.html", "utf-8")
+const js = Uglify.minify("src/twison.js")
 
 html = html.replace("{{SCRIPT}}", js.code)
 
-var outputJSON = {
+const outputJSON = {
   name: package.name,
   version: package.version,
   author: package.author,
@@ -16,11 +18,21 @@ var outputJSON = {
   source: html
 };
 
-var outputString = "window.storyFormat(" + JSON.stringify(outputJSON, null, 2) + ");";
+const outputString = "window.storyFormat(" + JSON.stringify(outputJSON, null, 2) + ");";
 fs.writeFile("dist/format.js", outputString, function(err) {
   if (err) { 
     console.log("Error building story format:", err);
   } else {
-    console.log("Successfully built story format to dist/format.js");
+
+		if (process.platform !== 'darwin') {
+			const srcdir = `"${__dirname}\\dist\\format.js"`
+			const targetdir = `"c:\\program files\\tweego\\story-formats\\tyf\\format.js"`
+			const cmd = `copy ${srcdir} ${targetdir}` 
+			exec(cmd, (err, stdout, stderr) => {
+				console.log('Rebuilt and copied to', targetdir)
+			})
+		}
+
+    console.log("successfully built story format to dist/format.js");
   }
 });
--- watch.js
+++ watch.js
@@ -5,24 +5,7 @@ console.log("Watching src directory for changes...");
 
 watch.watchTree(__dirname + '/src', function (f, curr, prev) {
   if (prev) {
-    console.log("Change detected, rebuilding.")
+    console.log("change detected, rebuilding.")
   }
 	childProcess.fork(__dirname + '/build.js');
-	
-	if (process.platform !== 'darwin') {
-		const exec = childProcess.exec
-
-		const cmd = (
-			`copy "${__dirname}\\dist\\format.js" "C:\\Users\\Alex Mutuc\\Desktop\\format.js"`
-			// "touch 'C:\\Users\\Alex Mutuc\\Desktop\\test.js'"
-		)
-		const dir = path.resolve(`${__dirname}/../../../`)
-		console.log(__dirname)
-		exec(cmd, { cwd: dir }, (err, stdout, stderr) => {
-			console.log("error", err)
-			console.log("stdout", stdout)
-			console.log('stderr', stderr)
-			console.log('Done.')
-		})
-	}
 });
