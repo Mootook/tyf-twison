374bd82f35fd1455bfbd47fa924b84a172aff6d7
Alex Mutuc <alexanderkmutuc@gmail.com>
2020-05-17 01:03:01 -0700

    Back to twee's naming convention, but abstracting out auto link from non-choice links

--- src/twison.js
+++ src/twison.js
@@ -1,22 +1,28 @@
 const choiceRegexp = /\[\[.+?\]\]/g
 const choiceSyntaxRegExp = /\[\[(.*?)\-\&gt;(.*?)\]\]/
 var Twison = {
+
+	/**
+	 *
+	 * @param {*} text
+	 */
   extractLinksFromText: function (text) {
-    var choices = text.match(choiceRegexp)
-    if (choices) {
-      return choices.map(function(link) {
+    var links = text.match(choiceRegexp)
+    if (links) {
+      return links.map(function(link) {
 				var differentName = link.match(choiceSyntaxRegExp);
         if (differentName) {
           // [[name->link]]
           return {
-            text: differentName[1],
+            name: differentName[1],
             link: differentName[2]
           };
         } else {
-          // [[link]]
-          link = link.substring(2, link.length-2)
+					// [[link]]
+					link = link.substring(2, link.length-2)
+					console.log("Direct Link", link)
           return {
-            text: link,
+            name: link,
             link: link
           }
         }
@@ -24,11 +30,15 @@ var Twison = {
     }
 	},
 	
+	/**
+	 *
+	 * @param {*} text
+	 */
 	filterText: function (text) {
 		let retVal = text
-		const choices = text.match(choiceRegexp) 
-		if (choices) {
-			choices.forEach(choice => {
+		const links = text.match(choiceRegexp) 
+		if (links) {
+			links.forEach(choice => {
 				retVal = retVal.replace(choice, '')
 			})
 		}
@@ -38,9 +48,13 @@ var Twison = {
 		return retVal.trim()
 	},
 
+	/**
+	 *
+	 * @param {*} dict
+	 */
 	arrangeKeys: function (dict) {
 		const retVal = {}
-		const orderedKeys = ['pid', 'name', 'text', 'choices']
+		const orderedKeys = ['pid', 'name', 'text', 'tags', 'auto_link', 'links']
 		orderedKeys.forEach(k => {
 			if (dict[k]) {
 				retVal[k] = dict[k]
@@ -49,62 +63,92 @@ var Twison = {
 		return retVal
 	},
 
+	/**
+	 *
+	 * @param {*} passage
+	 */
   convertPassage: function (passage) {
 		var text = passage.innerHTML
   	const dict = {}
-    var choices = Twison.extractLinksFromText(text);
-    if (choices) {
-      dict.choices = choices
-    }
-    ["name", "pid", "position", "tags"].forEach(function(attr) {
-      var value = passage.attributes[attr].value;
-      if (value) {
-        dict[attr] = value;
-      }
-    });
+		var linkMatch = text.match(choiceRegexp)
+    if (linkMatch) {
+			dict.links = linkMatch
+				.filter(l => {
+					const diffName = l.match(choiceSyntaxRegExp)
+					if (diffName) return true
+					else {
+						dict["auto_link"] = l.substring(2, l.length-2)
+						return false
+					}
+				})
+				.map(l => {
+					const k = l.match(choiceSyntaxRegExp)
+					return { name: k[1], link: k[2] }
+				})
+		}
+		if (!dict.links || !dict.links.length) {
+			delete dict.links
+		}
+		const dataArr = ['name', 'pid', 'position', 'tags']
+		dataArr.forEach(attr => {
+			const value = passage.attributes[attr].value
+			if (!value) return
+			dict[attr] = value
+		})
 
     if (dict.tags) {
       dict.tags = dict.tags.split(" ");
 		}
 		dict.text = Twison.filterText(text)
+
     return Twison.arrangeKeys(dict)
 	},
 
+	/**
+	 *
+	 * @param {*} story
+	 */
   convertStory: function(story) {
-    var passages = story.getElementsByTagName("tw-passagedata");
-    var convertedPassages = Array.prototype.slice.call(passages).map(Twison.convertPassage);
+    var passages = story.getElementsByTagName("tw-passagedata")
+    var convertedPassages = Array.prototype.slice.call(passages).map(Twison.convertPassage)
 
     var dict = {
       passages: convertedPassages
     };
 
-    ["name", "start_node", "ifid"].forEach(function(attr) {
-      var value = story.attributes[attr].value;
+    ["name", "startnode", "ifid"].forEach(function(attr) {
+      var value = story.attributes[attr].value
       if (value) {
-        dict[attr] = value;
+        dict[attr] = value
       }
-    });
+    })
 
     // Add PIDs to links
     var pidsByName = {};
-    dict.passages.forEach(function(passage) {
-      pidsByName[passage.name] = passage.pid;
-    });
+    dict.passages.forEach(passage => {
+      pidsByName[passage.name] = passage.pid
+		})
 
-    dict.passages.forEach(function(passage) {
+    dict.passages.forEach(passage => {
+			if (passage["auto_link"]) {
+				passage["auto_link"] = pidsByName[passage["auto_link"]]
+			}
       if (!passage.links) return;
       passage.links.forEach(function(link) {
         link.pid = pidsByName[link.link];
         if (!link.pid) {
           link.broken = true;
         }
-      });
-    });
+      })
+    })
 
 		dict.visited = false
-    return dict;
+    return dict
   },
 
+	/**
+	 *
+	 */
   convert: function() {
     var storyData = document.getElementsByTagName("tw-storydata")[0];
     var json = JSON.stringify(Twison.convertStory(storyData), null, 2);
